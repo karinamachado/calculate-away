6d91676f7a706c093c70465b4f73f46b9545d9d9
Karina Machado <karinamachado@msn.com>
2016-09-11 16:55:44 -0300

    update calculator exercise, thanks Kevin for class today :)

--- calcYouLaterAligator.js
+++ calcYouLaterAligator.js
@@ -8,3 +8,86 @@ whatIsThis("calculator"); //"This is a calculator"
 var myArray = [5,10,15,20]; 
 
 //YOUR CODE BELOW
+
+
+var add = function(num1, num2){
+
+return num1 + num2;
+
+};
+console.log(add(10,5));
+
+
+var addAll = function(){
+
+	var sumAll = myArray;
+
+	return sumAll
+}
+console.log(myArray);
+
+
+var subtract = function(num1,num2){
+  
+  return num3 - num4;
+
+}
+console.log(subtract(5,5));
+
+
+var diffFrom100 = function(num3){
+ 
+ return subtract - 100;
+
+}
+console.log(diffFrom100(50));
+
+
+var multiply = function(num1, num2){
+  return num1*num2;
+
+}
+console.log(multiply(10,5));
+
+var getTip - function(){
+
+   return multiply * 15 / 100
+}
+console.log(getTip(6,7));
+
+
+var divide = function(num1,num2){
+
+	return num1/num2;
+};
+console.log(divide(10,5));
+
+var getHalf = function(num4){
+
+  return divide/num4;
+}
+console.log(getHalf(5));
+
+var isPositive = function(num){
+
+if(num > 0){
+
+	console.log( " Is true, The number os greater than 0, ");
+
+
+}else{
+
+	console.log(" Is False");
+}
+
+}
+
+var getRemainder = function(){
+	
+}
+
+
+
+
+
+
